e645ebd9a93c9529633c07bb7937523c6a8aea3f
Kris Nee <kristi.neemesto@gmail.com>
2025-05-26 17:29:47 +0300

    Add OpenWeatherServices to fetch weather data
    Introduces the `OpenWeatherServices` class implementing the `IOpenWeatherServices` interface. This service retrieves weather information from the OpenWeather API using city names provided in `OpenWeatherResultDto`. It deserializes the JSON response into `OpenWeatherRootDto` and related DTOs, which structure the API response. The `OpenWeatherResultDto` holds the processed weather data, including temperature, humidity, and descriptions. Necessary namespaces have been imported to support this functionality.

---
+++ ShopTARge23/ShopTARge23.Core/Dto/OpenWeatherDto/AccuWeatherRootDto.cs
@@ -0,0 +1,88 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ShopTARge23.Core.Dto.WeatherDtos.OpenWeatherDtos
+{
+    public class OpenWeatherRootDto
+    {
+        [JsonPropertyName("coord")]
+        public CoordDto Coord { get; set; }
+
+        [JsonPropertyName("weather")]
+        public List<WeatherDto> Weather { get; set; }
+
+        [JsonPropertyName("main")]
+        public MainDto Main { get; set; }
+
+        [JsonPropertyName("wind")]
+        public WindDto Wind { get; set; }
+
+        [JsonPropertyName("clouds")]
+        public CloudsDto Clouds { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("cod")]
+        public int Cod { get; set; }
+    }
+
+    public class CoordDto
+    {
+        [JsonPropertyName("lon")]
+        public double Lon { get; set; }
+
+        [JsonPropertyName("lat")]
+        public double Lat { get; set; }
+    }
+
+    public class WeatherDto
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("main")]
+        public string Main { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("icon")]
+        public string Icon { get; set; }
+    }
+
+    public class MainDto
+    {
+        [JsonPropertyName("temp")]
+        public double Temp { get; set; }
+
+        [JsonPropertyName("feels_like")]
+        public double FeelsLike { get; set; }
+
+        [JsonPropertyName("temp_min")]
+        public double TempMin { get; set; }
+
+        [JsonPropertyName("temp_max")]
+        public double TempMax { get; set; }
+
+        [JsonPropertyName("pressure")]
+        public int Pressure { get; set; }
+
+        [JsonPropertyName("humidity")]
+        public int Humidity { get; set; }
+    }
+
+    public class WindDto
+    {
+        [JsonPropertyName("speed")]
+        public double Speed { get; set; }
+
+        [JsonPropertyName("deg")]
+        public int Deg { get; set; }
+    }
+
+    public class CloudsDto
+    {
+        [JsonPropertyName("all")]
+        public int All { get; set; }
+    }
+}
